1fdbe214a8c6e35e34b0ae8b10d30a2b301c1d34
armand476 <armand.guglielmini@schola-africa.org>
2024-12-16 15:26:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,24 +9,32 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+st.markdown("<hr style='border:1px solid black;'>", unsafe_allow_html=True)
+st.markdown(
+    """
+    <div style="text-align: center;text-decoration:underline;font-size:20px;">
+        Bilan :
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+age =  st.number_input("Age :", min_value=0, max_value=1, step=1)
 température = st.number_input("Température :", min_value=35.0, max_value=45.0, step=0.1)
-age = st.selectbox(
-        "Age", 
-        options=np.arange(0, 130, 1).tolist()
-    )
-sat = st.selectbox(
-        "Saturation en oxygéne", 
-        options=np.arange(60, 100, 0.1).tolist()
-    )
+sat = st.number_input("Saturation d'oxygéne :", min_value=0.0, max_value=100.0, step=0.1)
 cols=st.columns(2)
 with cols[0]:
-    t1 = st.selectbox(
-        "Tension systollique", 
-        options=np.arange(10, 200, 1)
-    )
+    t1 = st.number_input("Tension systollique :", min_value=0, max_value=300, step=1)
 with cols[1]:
-        
-    t2 = st.selectbox(
-        "Tension diastollique", 
-        options=np.arange(10, 200, 1)
-    )
+    t2 = st.number_input("Tension systollique :", min_value=0, max_value=300, step=1)   
+st.markdown("<hr style='border:1px solid black;'>", unsafe_allow_html=True)
+st.markdown(
+    """
+    <div style="text-align: center;text-decoration:underline;font-size:20px;">
+        Description symptome :
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+symptome= st.text_input("Symptome :")
+antécédents= st.text_input("Symptome :")
+   
