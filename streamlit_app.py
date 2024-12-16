bcdea73dc4028851667f9878b2d925f532f9dd36
armand476 <armand.guglielmini@schola-africa.org>
2024-12-16 15:38:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,14 +18,25 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-age =  st.number_input("Age :", min_value=0, max_value=1, step=1)
+colage=st.columns(2)
+with colage[0]:
+    age =  st.number_input("Age :", min_value=0, max_value=1, step=1)
+with colage[1]:
+    typeage = st.selectbox(
+        "Unité age", 
+        options=["ans", "mois"], 
+        key="unité_age"
+    )
 température = st.number_input("Température :", min_value=35.0, max_value=45.0, step=0.1)
 sat = st.number_input("Saturation d'oxygéne :", min_value=0.0, max_value=100.0, step=0.1)
 cols=st.columns(2)
 with cols[0]:
     t1 = st.number_input("Tension systollique :", min_value=0, max_value=300, step=1)
 with cols[1]:
-    t2 = st.number_input("Tension systollique :", min_value=0, max_value=300, step=1)   
+    t2 = st.number_input("Tension diastollique :", min_value=0, max_value=300, step=1)   
+st.markdown("<hr style='border:1px solid black;'>", unsafe_allow_html=True)
+
+symptome= st.text_input("Autres mesures :")
 st.markdown("<hr style='border:1px solid black;'>", unsafe_allow_html=True)
 st.markdown(
     """
