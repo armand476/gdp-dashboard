5af7d42ce8533f18431f91f4b5506c9d7165b01a
armand476 <armand.guglielmini@schola-africa.org>
2024-12-19 10:30:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import requests
 import os
 from openai import OpenAI
 diagnostique=[]
-image_url = "https://github.com/armand476/gdp-dashboard/blob/main/467483315_1774408906704135_3086879669903639807_n.jpg?raw=true"
+image_url = "https://github.com/armand476/gdp-dashboard/blob/main/462584175_1119871882966319_8595602765645332567_n.jpg?raw=true"
 if not 'question' in st.session_state:
     st.session_state['question']=[]
     st.session_state['réponse']=[]
